567d4312940b7d5dc094697c0cc4481980582a49
tao.zhu <zhutao@live.cn>
2019-10-15 21:08:19 +0800

    d.ts test v1

--- types/global.d.ts
+++ types/global.d.ts
@@ -1,24 +1,26 @@
+import { execFileSync } from "child_process";
 
-
+export as namespace efs;
 type path  = string;
 type depth = number;
 type callback = () => void;
 type isBoolean = boolean;
 
-export namespace efs {
+export = efs;
+declare interface efs {
 
-  function eachDir (config : {
+  eachDir: (config : {
     depth: depth, 
     _path: path, 
     forEachCb: callback, 
     cb: callback
-  }): void
-
-  function cat (config: {
+  }) => void;
+  
+   cat: (config: {
     _path: path, 
     ext: string[],
     cb: callback, 
     isClearComment: isBoolean
-  }): string|undefined
+  }) => string|undefined
 
 }
